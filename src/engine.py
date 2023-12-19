e71485496234e8e3a24a8609d5f4b5658320f6f2
nsfogg <nsfogg3@gmail.com>
2023-12-18 21:46:44 -0500

    Debugging ab pruning

--- src/engine.py
+++ src/engine.py
@@ -3,14 +3,17 @@
 # Chess engine logic
 
 from tree import *
+import chess
+import chess.engine
 
-class engine():
-    def __init__(self):
+class Engine():
+    def __init__(self, position):
         self.search_bound = 3
-        node = TreeNode()
+        node = TreeNode(position)
         self.turn = True
-
-        self.alpha_beta_minimax(self, node, a, b)
+        self.best_move = []
+        self.eng = chess.engine.SimpleEngine.popen_uci("D:\Projects\Stockfish\stockfish-windows-x86-64-modern")
+        self.alpha_beta_minimax(node, float('-inf'), float('inf'))
     
     def alpha_beta_minimax(self, node, alpha, beta):
         # check if at search bound
@@ -20,13 +23,13 @@ class engine():
         # check if at leaf
         if len(node.children) == 0:
             if node.get_level() == 0:
-                bestMove = []
+                self.bestMove = []
             return self.static_eval(node)
 
         # init best moves list
         if node.get_level() == 0:
-            best_move = node.children[0]
-            # ccheck if there is only one option
+            self.best_move.append(node.children[0])
+            # check if there is only one option
             if len(node.children) == 1:
                 return None
         
@@ -38,7 +41,7 @@ class engine():
                 if result > alpha:
                     alpha = result
                     if node.get_level == 0:
-                        best_move = child
+                        self.best_move.append(child)
                 if alpha >= beta:
                     return alpha
             return alpha
@@ -50,10 +53,37 @@ class engine():
                 if result < beta:
                     beta = result
                     if node.get_level() == 0:
-                        best_move = child
+                        self.best_move.append(child)
                 if beta <= alpha:
                     return beta
             return beta
     
     def static_eval(self, node):
-        return 0
+        info = self.eng.analyse(node, chess.engine.Limit(time=0.1))
+        score = info['score']
+        num = ""
+        mate = False
+        if score[0:4] == 'Mate':
+            Mate = True
+            i = score[5]
+            while not i == ')':
+                num += score[i]
+        else:
+            i = score[3]
+            while not i == ')':
+                num += score[i]
+        
+        if mate:
+            if num[0] == '-':
+                return int(num) - 100000
+            else:
+                return int(num) + 100000
+        else:
+            return int(num)
+
+if __name__ == '__main__':
+    board = chess.Board()
+    board.push_san("f3")
+    board.push_san("e6")
+    eng = Engine(board)
+    print(eng.best_move)
